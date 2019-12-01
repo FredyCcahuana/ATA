0e8da9d93abd46a665050bf4470aa5ba3953ceea
FredyCcahuana <fr3dy.ccahuana@gmail.com>
2019-12-01 03:46:08 -0500

    inicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
